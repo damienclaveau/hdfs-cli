27663f851e65ea81cf2c55595f8c5867d0f50ab6
damien.claveau <damien.claveau@edf.fr>
2015-10-27 11:53:18 +0100

    Support for kerberos principal rewrite rules declared in hadoop.security.auth_to_local (Load core-site.xml and hdfs-site.xml from HADOOP_CONF_DIR)

--- src/main/java/com/instanceone/hdfs/shell/command/HdfsConnect.java
+++ src/main/java/com/instanceone/hdfs/shell/command/HdfsConnect.java
@@ -4,9 +4,10 @@ package com.instanceone.hdfs.shell.command;
 
 import java.io.IOException;
 import java.net.URI;
+import java.io.File;
 
-import com.instanceone.hdfs.shell.completers.FileSystemNameCompleter;
 import com.instanceone.stemshell.command.AbstractCommand;
+import com.instanceone.stemshell.Environment;
 import jline.console.ConsoleReader;
 import jline.console.completer.Completer;
 import jline.console.completer.StringsCompleter;
@@ -15,12 +16,13 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-
-import com.instanceone.stemshell.Environment;
 import org.apache.hadoop.security.UserGroupInformation;
 
 public class HdfsConnect extends AbstractCommand {
-
+    
+    public static final String HADOOP_CONF_DIR = "HADOOP_CONF_DIR";
+    private static final String[] HADOOP_CONF_FILES = {"core-site.xml", "hdfs-site.xml"};
+    
     public HdfsConnect(String name) {
         super(name);
         Completer completer = new StringsCompleter("hdfs://localhost:8020/", "hdfs://hdfshost:8020/");
@@ -30,7 +32,16 @@ public class HdfsConnect extends AbstractCommand {
     public void execute(Environment env, CommandLine cmd, ConsoleReader reader) {
         try {
             if(cmd.getArgs().length > 0){
-                Configuration config = new Configuration();
+                Configuration config = new Configuration(false);
+                
+                String hadoopConfDirProp = System.getenv().get(HADOOP_CONF_DIR);
+                File hadoopConfDir = new File(hadoopConfDirProp).getAbsoluteFile();
+                for (String file : HADOOP_CONF_FILES) {
+                  File f = new File(hadoopConfDir, file);
+                  if (f.exists()) {
+                    config.addResource(new Path(f.getAbsolutePath()));
+                  }
+                }
 
                 if (env.getProperty(HdfsKrb.USE_KERBEROS) != null && env.getProperty(HdfsKrb.USE_KERBEROS) == "true") {
                     config.set(HdfsKrb.HADOOP_AUTHENTICATION, HdfsKrb.KERBEROS);
